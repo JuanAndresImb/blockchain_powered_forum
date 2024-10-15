12da2f3b7cd805509fb108762e07d168c94beede
Juan Andres <j.imbaquingo@outlook.fr>
2024-10-15 21:53:15 +0200

    Server_structure

---
+++ app.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+cost session = require('express-session');
+const bodyParser = require('body-parser');
+
+const app = express(); 
+
+//sessions config
+
+app.use(session({
+    secret: 'llavePrivada89';
+    resave: false,
+    saveUninitialized: true, // A session is uninitialized when it is new but not modified. Choosing false is useful for implementing login sessions, reducing server storage usage, or complying with laws that require permission before setting a cookie.
+    cookie: { secure: false }
+}));
+
+//body-parser config
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//importing routes
+const authRoutees = require('./(routes/authRoutes');
+const newRoutes = require('./routes/newRoutes');
+const verificationRoutes = require('./routes/verificationRoutes');
+
+//using routes
+
+app.use('./auth', authRoutees);
+app.use('./news', newRoutes);
+app.use('./verification', verificationRoutes);
+
+//Server Port
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+});
